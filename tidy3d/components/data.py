6f529e244431b1e8d1ee1bf750ac3cd0a8715ee5
Shashwat Sharma <shashwat@flexcompute.com>
2022-03-31 14:49:50 -0400

    minor change to accommodate complex time monitor data

--- tidy3d/components/data.py
+++ tidy3d/components/data.py
@@ -547,8 +547,7 @@ class ScalarFieldTimeData(AbstractScalarFieldData, TimeData):
     >>> data = ScalarFieldTimeData(values=values, x=x, y=y, z=z, t=t)
     """
 
-    # values: Union[Array[float], Array[complex]]
-    values: Array[complex]
+    values: Union[Array[float], Array[complex]]
     data_attrs: Dict[str, str] = None  # {'units': '[E] = V/m, [H] = A/m'}
     type: Literal["ScalarFieldTimeData"] = "ScalarFieldTimeData"
 
