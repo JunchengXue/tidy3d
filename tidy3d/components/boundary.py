29cbfd9db83d325e46b81b17872ec9bee46b6c9a
Shashwat Sharma <shashwat@flexcompute.com>
2022-04-04 18:00:55 -0400

    minor changes to the temporary file boundary.py which is a prototype for the new way of defining boundary conditions

--- tidy3d/components/boundary.py
+++ tidy3d/components/boundary.py
@@ -12,15 +12,24 @@ from .base import Tidy3dBaseModel
 # class Boundary(Tidy3dBaseModel):
 
 
-
 Boundary = Literal["PBC", "PEC", "PMC", "Bloch"]
 
 # types of boundaries that are accepted by simulation
 BoundaryType = Union[
-    Boundary, Symmetry, PMLTypes
-]
+    Boundary, PMLTypes
+    ]
 
 # boundary definition at the min and max coordinates along one dimension
-Boundary1D = Tuple[BoundaryType, BoundaryType]
+Boundary1D = Union[
+    Tuple[BoundaryType, BoundaryType]
+    ]
 
+# Logical constraints on Boundary1D:
+# - If one entry is PBC, the other must also be PBC
+# - If one entry is Bloch, the other must also be Bloch
+# - If the BC is either PBC or Bloch, there cannot be a PML along that dimension
+# - If the BC is either PBC or Bloch, there cannot be a symmetry specified along that dimension
+# - PML overrides any non-periodic boundary conditions (PEC, PMC) along that dimension
+# - PML, PEC, and PMC can be used in any combination
+# - PEC and PMC are synonymous with symmetry once the domain is truncated
 
